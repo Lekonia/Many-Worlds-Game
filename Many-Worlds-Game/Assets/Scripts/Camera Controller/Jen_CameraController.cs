7bf2afade32930e7e6ec9ab27fed1cd9c82ee021
Andrew P Gonzalez <Lekonia@iinet.net.au>
2020-12-08 20:35:37 +1100

    Jen_CameraController
    the script works up to this point, but it does not work well with orthographic but it works and the character can still move

--- Many-Worlds-Game/Assets/Scripts/Camera Controller/Jen_CameraController.cs
+++ Many-Worlds-Game/Assets/Scripts/Camera Controller/Jen_CameraController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class Jen_CameraController : MonoBehaviour
@@ -7,11 +8,19 @@ public class Jen_CameraController : MonoBehaviour
     public Transform target;
 
     public Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float minZoom = 5f;
+    public float maxZoom = 15f;
 
     public float pitch = 2f;
 
     private float currentZoom = 10f;
 
+    void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+    }
     void LateUpdate()
     {
         transform.position = target.position - offset * currentZoom;
