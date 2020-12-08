f4845646a1c2ff7f99a240b49cc947e1574be766
Andrew P Gonzalez <Lekonia@iinet.net.au>
2020-12-08 21:04:37 +1100

    It all works
    for the most part, the Jen_CameraController works perfectly in perspective, not so much orthographic, need to find an alternative/fix for this, but it works!

--- Many-Worlds-Game/Assets/Scripts/Camera Controller/Jen_CameraController.cs
+++ Many-Worlds-Game/Assets/Scripts/Camera Controller/Jen_CameraController.cs
@@ -14,16 +14,23 @@ public class Jen_CameraController : MonoBehaviour
 
     public float pitch = 2f;
 
+    public float yawSpeed = 100f;
+
     private float currentZoom = 10f;
+    private float currentYaw = 0f;
 
     void Update()
     {
         currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
         currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
     }
     void LateUpdate()
     {
         transform.position = target.position - offset * currentZoom;
         transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
     }
 }
