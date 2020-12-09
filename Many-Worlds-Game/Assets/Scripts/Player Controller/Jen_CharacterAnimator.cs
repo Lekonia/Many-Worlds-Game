b03ff339910359d963a59c7cc134fe9fc5f3ae22
Andrew P Gonzalez <Lekonia@iinet.net.au>
2020-12-09 19:51:50 +1100

    Animation was a success

---
+++ Many-Worlds-Game/Assets/Scripts/Player Controller/Jen_CharacterAnimator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Jen_CharacterAnimator : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = .1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>(); // This line here may have some difficulties, based on where the animator is placed and it may be a parent, so try without 'InChildre'
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
