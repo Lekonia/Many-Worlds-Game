30950d0b33a91c48bd14412bd26bd85b50153a29
Andrew P Gonzalez <Lekonia@iinet.net.au>
2020-12-10 14:31:26 +1100

    Item pick up works to this point

--- Many-Worlds-Game/Assets/Scripts/Item Pickup/Jen_ItemPickup.cs
+++ Many-Worlds-Game/Assets/Scripts/Item Pickup/Jen_ItemPickup.cs
@@ -2,5 +2,17 @@
 
 public class Jen_ItemPickup : Jen_Interactable
 {
+    public override void Interact()
+    {
+        base.Interact();
 
+        Pickup();
+    }
+
+    void Pickup()
+    {
+        Debug.Log("Picking up item.");
+        // Add to inventory
+        Destroy(gameObject);
+    }
 }
