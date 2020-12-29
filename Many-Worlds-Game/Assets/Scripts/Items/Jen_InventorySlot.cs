cbf784224cc2d6c033b6d73942b52ee13d8db9e3
Andrew P Gonzalez <Lekonia@iinet.net.au>
2020-12-29 19:14:56 +1100

    End of creating an inventory system

---
+++ Many-Worlds-Game/Assets/Scripts/Items/Jen_InventorySlot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Jen_InventorySlot : MonoBehaviour
+{
+    public Image icon;
+    public Button removeButton;
+
+    Jen_Item item;
+
+    public void AddItem (Jen_Item newItem)
+    {
+        item = newItem;
+
+        icon.sprite = item.icon;
+        icon.enabled = true;
+        removeButton.interactable = true;
+    }
+
+    public void ClearSlot()
+    {
+        item = null;
+
+        icon.sprite = null;
+        icon.enabled = false;
+        removeButton.interactable = false;
+    }
+
+    public void OnRemoveButton()
+    {
+        Jen_Inventory.instance.Remove(item);
+    }
+
+    public void UseItem ()
+    {
+        if (item != null)
+        {
+            item.Use();
+        }
+    }
+}
