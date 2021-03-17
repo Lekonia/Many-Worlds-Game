25534e5b6db10fe60adbc763f2c838d0fbddad57
Andrew P Gonzalez <Lekonia@iinet.net.au>
2021-03-17 17:56:13 +1100

    Implement a test scene with the growth script

---
+++ Many-Worlds-Game/Assets/Scripts/GrowthBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowthBehaviour : MonoBehaviour
+{
+    public GameObject[] Item;
+    public float Time;
+
+    void Start()
+    {
+        StartCoroutine("enable");
+        //SendMessage("enable");
+        //gameObject.SendMessage("enable");
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator enable()
+    {
+        yield return new WaitForSeconds(Time);
+        Item[0].SetActive(true);
+        yield return new WaitForSeconds(Time);
+        Item[0].SetActive(false);
+        Item[1].SetActive(true);
+        yield return new WaitForSeconds(Time);
+        Item[1].SetActive(false);
+        Item[2].SetActive(true);
+        yield return new WaitForSeconds(Time);
+        Item[2].SetActive(false);
+        Item[3].SetActive(true);
+    }
+}
